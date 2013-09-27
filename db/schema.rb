d0314a04f7c7eede52e588c8daebeda1a802c0b8
Brandon Rodgers <brandonrodgers@gmail.com>
2013-09-27 19:34:17 -0400

    Changed the look of the profile page and added favorites

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908013037) do
+ActiveRecord::Schema.define(:version => 20130927211447) do
 
   create_table "installs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(:version => 20130908013037) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "favorite_movie"
+    t.string   "favorite_book"
+    t.string   "favorite_song"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
